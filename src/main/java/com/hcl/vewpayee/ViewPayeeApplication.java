d7d7c343ec5730d064947d093c033099ab0ae46e
User1 <User1@LP-CNU3099WJB.HCLT.CORP.HCL.IN>
2019-05-06 17:44:17 +0530

    shipra use case

---
+++ src/main/java/com/hcl/vewpayee/ViewPayeeApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.vewpayee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewPayeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewPayeeApplication.class, args);
+	}
+
+}
